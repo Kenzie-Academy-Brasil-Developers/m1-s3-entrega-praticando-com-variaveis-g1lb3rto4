67a3829202ed04fda1def452e264cf22e96a6a9a
Gilberto <gilbertooliveira266@gmail.com>
2022-06-13 14:17:41 -0300

    entrega s3-11

---
+++ S3-11.js
@@ -0,0 +1,13 @@
+let nome = "Gilberto "
+
+let sobreNome = "Oliveira "
+
+let anoNascimento = 1998
+
+let cidade = "Santo-Ângelo"
+
+let anoAtual = 2022
+
+let idade = anoAtual - anoNascimento
+
+console.log("Eu sou o " + nome + sobreNome + "tenho " + idade + " anos e moro em " + cidade);
